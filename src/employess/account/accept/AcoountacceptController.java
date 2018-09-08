805b95f2cb6a2c11ca7aae9d7d2a36e6fc644494
NourAshraf211 <nourashraf.fcih@gmail.com>
2018-09-08 17:09:15 +0200

    update3

--- src/employess/account/accept/AcoountacceptController.java
+++ src/employess/account/accept/AcoountacceptController.java
@@ -5,16 +5,12 @@
  */
 package employess.account.accept;
 
-import com.jfoenix.controls.JFXButton;
 import employees.main.EmployeesController;
-import java.io.IOException;
+import com.jfoenix.controls.JFXButton;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
 
@@ -25,12 +21,13 @@ import javafx.scene.layout.AnchorPane;
  */
 public class AcoountacceptController implements Initializable {
     EmployeesController x = new EmployeesController();
+
     @FXML
     private AnchorPane loadPane;
     @FXML
-    private JFXButton accountaccept;
-    @FXML
     private JFXButton personalexpenses;
+    @FXML
+    private JFXButton accountacceptbutton;
 
     /**
      * Initializes the controller class.
@@ -42,31 +39,17 @@ public class AcoountacceptController implements Initializable {
 
     @FXML
     private void loadAccountAccept(ActionEvent event) {
-        //loadWindow("/employees/account/accept/attendence/attendence.fxml");
         x.loadwindow(loadPane,"/employees/account/accept/attendence/attendence.fxml");
     }
 
     @FXML
     private void loadPersonalExpenses(ActionEvent event) {
-        //loadWindow("/employees/personalexpenses/personalexpenses.fxml");
         x.loadwindow(loadPane,"/employees/personalexpenses/personalexpenses.fxml");
     }
 
     @FXML
     private void loadBack(ActionEvent event) {
-        //loadWindow("/employees/account/account.fxml");
         x.loadwindow(loadPane,"/employees/account/account.fxml");
     }
     
-   /* void loadWindow(String loc)
-    {
-        try {
-            AnchorPane pane = FXMLLoader.load(getClass().getResource(loc));
-            loadPane.getChildren().setAll(pane);
-           
-        } catch (IOException ex) {
-            Logger.getLogger(EmployeesController.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }*/
-
 }
